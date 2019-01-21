2ad348c015ec08d1fd040f645b66abf4eaafba40
Akram Helil <akramhelil@gmail.com>
2019-01-21 21:47:07 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,9 +2,9 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     keys_of_hash = []
-    arguments.each do |arg|
+    arguments.each do |argument|
       map do |key, value|
-        if arg == value
+        if argument == value
           keys_of_hash << key
         end
       end
